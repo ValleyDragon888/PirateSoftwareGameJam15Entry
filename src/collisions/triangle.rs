890187ceea34d6718650d3ecbecb69603c47624b
valleydragon888 <valleydragon888@gmail.com>
2024-06-14 17:30:31 +0100

    add draw function to triangle

--- src/collisions/triangle.rs
+++ src/collisions/triangle.rs
@@ -1,7 +1,15 @@
+use macroquad::color::Color;
 use macroquad::math::Vec2;
+use macroquad::shapes::draw_triangle;
 use crate::collisions::line::Line;
 
 struct Triangle {
     pub lines: [Line; 3],
     pub points: [Vec2; 3]
+}
+
+impl Triangle {
+    pub fn draw(&self, color: Color) {
+        draw_triangle(self.points[0], self.points[1], self.points[2], color)
+    }
 }
