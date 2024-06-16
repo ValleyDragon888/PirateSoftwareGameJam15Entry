94bf6967c4d4ecfffcb3b43657c7f6037ea7c4b0
valleydragon888 <valleydragon888@gmail.com>
2024-06-16 16:58:05 +0100

    add loading texture, add assets to wasm build

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,14 @@ use collisions::line::Line;
 async fn main() {
     let mut x = 0f32;
     let line = Line::new(0f32, 0f32, 20f32, 20f32);
+    let texture: Texture2D = load_texture("assets/thor-highquality.png").await.unwrap();
     loop {
         clear_background(RED);
         draw_circle(x, screen_height() - 30.0, 15.0, YELLOW);
         x += 1f32;
 
         draw_text("IT WORKS!", 20.0, 20.0, 30.0, DARKGRAY);
+        draw_texture(&texture, 100., 100., WHITE);
         line.draw(10.0, GREEN);
         next_frame().await
     }
