75070688d074d1a9968596ea760aac98ee5defb8
ValleyDragon888 <valleydragon888@gmail.com>
2024-07-06 18:58:38 +0100

    begin working on own ecs, this will overwrite changes on laptop

--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,32 @@
-#![feature(tuple_trait)]
-
 use macroquad::prelude::*;
-use bevy_ecs::prelude::*;
-mod collisions;
-mod maths;
-mod app;
-use app::App;
 
-#[derive(Component, Debug)]
-struct Position { x: f32, y: f32 }
-
-#[derive(Component)]
-struct Circle {}
-
-fn hello() {
-    println!("Hello")
+struct Schedule<'a> {
+    systems: &'a Vec<&'a dyn Fn() -> ()>
 }
 
-fn hi() {
-    println!("Hi")
-}
-
-fn create_circle(
-    mut world: &mut World
-) {
-    world.spawn((
-        Position {x: 0.0, y:0.0},
-        Circle {}
-    ));
-}
-
-fn print_circles(
-    mut query: Query<&mut Position, With<Circle>>
-) {
-    for i in query.iter_mut() {
-        println!("Found circle at {:?}", i)
+impl<'a> Schedule<'a> {
+    fn run(&'a mut self) {
+        let systems = self.systems;
+        for func in systems {
+            func();
+        }
     }
 }
 
-fn render_circles(
-    query: Query<&Position, With<Circle>>
-) {
-    for i in query.iter() {
-        draw_circle(i.x, i.y, 200.0, RED);
-    }
+fn sys_1() {
+    println!("sys 1 run");
 }
 
-fn background() {
-    clear_background(GREEN);
-}
-
-fn move_circles(
-    mut query: Query<&mut Position, With<Circle>>
-) {
-    for mut pos in query.iter_mut() {
-        pos.x += 20.0 * get_frame_time()
-    }
+fn sys_2() {
+    println!("sys 2 run");
 }
 
 #[macroquad::main("Desktop Build of Entry")]
 async fn main() {
-    let mut app = App::new();
-    app.startup_schedule.add_systems(create_circle);
-    app.update_schedule.add_systems(move_circles);
-    app.render_schedule.add_systems((background, render_circles).chain());
-    app.run().await
+    let mut w = Schedule { systems: &vec!(&sys_1, &sys_2) };
+    w.run();
+    loop {
+        draw_circle(0.0, 0.0, 200.0, RED);
+        next_frame().await;
+    }
 }
