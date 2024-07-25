5783e46789f4e6f9de6e740f3fddf6872f4bd617
valleydragon888 <valleydragon888@gmail.com>
2024-07-25 17:03:52 +0100

    YOU CAN DIE NOW.

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,30 @@
 mod sprite;
 
+use std::process::exit;
 use cooldown::Cooldown;
 use macroquad::prelude::*;
 use sprite::Player;
+use crate::GameState::Playing;
 use crate::maths::vec2s;
 use crate::sprite::Enemy;
 
 mod maths;
 mod cooldown;
 
+pub enum GameState {
+    Playing,
+    Dead(DeathReason)
+}
+
+pub enum DeathReason {
+    LanternRanOut,
+    HealthRanOut
+}
 
 #[macroquad::main("Desktop Build of Entry")]
 async fn main() {
-    let texture: Texture2D = load_texture("assets/thor-highquality.png").await.unwrap();
-    let mut player = Player {
-        pos: vec2s(0.1, 0.1),
-        texture2d: texture,
-        health: 50,
-        lantern_capacity: 50
-    };
+    let mut game_state = GameState::Playing;
+    let mut player = Player::new();
     let texture: Texture2D = load_texture("assets/thor-highquality.png").await.unwrap();
     let mut enemy = Enemy {
         pos: vec2s(0.1, 0.2),
@@ -35,10 +41,67 @@ async fn main() {
         // let camera_size = view_size + remaining_size;
         //
         // set_camera(&Camera2D::from_display_rect(Rect::new( 0.0, camera_size.y, camera_size.x, -camera_size.y)));
+        match game_state {
+            GameState::Playing => {
+                clear_background(WHITE);
+                player.update();
+                enemy.update(&mut player);
+                next_frame().await;
+                if player.health <= 0  { game_state = GameState::Dead(DeathReason::HealthRanOut) }
+                else if player.lantern_capacity <= 0  { game_state = GameState::Dead(DeathReason::LanternRanOut) }
+            }
+            GameState::Dead(ref reason) => {
+                do_death_screen(reason).await;
+                game_state = Playing;
+                player = Player::new()
+            }
+        }
 
-        clear_background(WHITE);
-        player.update();
-        enemy.update(&mut player);
+    }
+}
+
+async fn do_death_screen(reason: &DeathReason) {
+    loop {
+        clear_background(BLACK);
+
+        // You Died
+        draw_text_center("You Died.", 100.0, 100, WHITE);
+
+        // Death specific messasge
+        let message = match reason {
+            DeathReason::LanternRanOut => {"You let your lantern run out of juice."}
+            DeathReason::HealthRanOut => {"Your health ran out."}
+        };
+        draw_text_center(message, 250.0, 50, WHITE);
+        draw_text_center("\"Maybe stay alive next time.\"", 270.0, 30, WHITE);
+
+        // Buttons
+        let respawn_size = draw_text_center("Re-Spawn", 350.0, 100, BLUE);
+        let rage_quit_size = draw_text_center("Rage Quit", 420.0, 100, BLUE);
+
+        let respawn_rect = Rect::new(respawn_size.1, 300.0, respawn_size.0.width, respawn_size.0.height);
+        let rage_quit_rect = Rect::new(rage_quit_size.1, 370.0, rage_quit_size.0.width, rage_quit_size.0.height);
+
+        if respawn_rect.contains(Vec2::from(mouse_position())) && is_mouse_button_down(MouseButton::Left) { break }
+        if rage_quit_rect.contains(Vec2::from(mouse_position())) && is_mouse_button_down(MouseButton::Left) { exit(0); }
+
         next_frame().await;
     }
+}
+
+fn center_pos(width:f32) -> f32 {
+    return (screen_width() / 2.0) - (width / 2.0)
+}
+
+fn draw_text_center(text: &str, y:f32, font_size:i8, color: Color) -> (TextDimensions, f32) {
+    // f32 return is the center_pos of text
+    let text_size = measure_text(
+        text, Option::None, font_size as u16, 1.0
+    );
+    draw_text(text,
+              center_pos(text_size.width),
+              y,
+              font_size as f32,
+              color);
+    return (text_size, center_pos(text_size.width))
 }
--- src/sprite.rs
+++ src/sprite.rs
@@ -1,11 +1,12 @@
 use macroquad::color::{BLACK, BLUE, DARKBLUE, MAROON, RED};
 use macroquad::input::{is_key_down, KeyCode};
-use macroquad::prelude::{draw_circle, draw_texture, Texture2D, WHITE};
+use macroquad::prelude::{draw_circle, draw_texture, load_texture, Texture2D, WHITE};
 use macroquad::shapes::draw_rectangle;
 use macroquad::text::draw_text;
 use macroquad::window::{screen_height, screen_width};
 use crate::cooldown::Cooldown;
-use crate::maths::Vec2s;
+use crate::maths::{Vec2s, vec2s};
+use futures::executor::block_on;
 
 pub struct Player {
     pub pos:Vec2s,
@@ -52,6 +53,17 @@ impl Player {
         // I still hate this hack, but this time it's copied code AS WELL!
         draw_text(&("Lantern Capacity: ".to_owned()+&self.lantern_capacity.to_string()+"%")
                   , screen_width()/2.0, screen_height()-4.0, 28.0, BLACK);
+    }
+
+    pub(crate) fn new() -> Player {
+        let mut texture_future = load_texture("assets/thor-highquality.png");
+        let texture = block_on(texture_future).expect("TODO: wat de frick");
+        return Player {
+            pos: vec2s(0.1, 0.1),
+            texture2d: texture,
+            health: 50,
+            lantern_capacity: 50
+        };
     }
 }
 
